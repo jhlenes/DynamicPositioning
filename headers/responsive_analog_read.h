53c68a2a455234c8ee8e3b8eef3e67d9f6e56248
Jan Henrik Lenes <jhlenes@outlook.com>
2017-03-20 12:03:54 +0100

    Add good comments in responsive_analog_read

--- headers/responsive_analog_read.h
+++ headers/responsive_analog_read.h
@@ -1,7 +1,6 @@
 #ifndef RESPONSIVE_ANALOG_READ_H_
 #define RESPONSIVE_ANALOG_READ_H_
 
-#include <stdlib.h>	// Function: abs()
 int responsive_analog_read(int newValue);
 
 #endif /* RESPONSIVE_ANALOG_READ_H_ */
--- responsive_analog_read.c
+++ responsive_analog_read.c
@@ -1,47 +1,95 @@
-/*
+/**************************************************
  *
- * Implementation of a noise-reduction algorithm. It uses the exponential moving average
- * to reduce the noise. It also ensures responsiveness by reducing accuracy
- * when it is not needed, i.e. when the value changes rapidly.
+ * FILENAME:	responive_analog_read.c
  *
- */
+ * DESCRIPTION:
+ * 			Implementation of a noise-reduction algorithm. It uses the method of exponential moving average
+ * 			to reduce the noise. It's a very simple algorithm that behaves similar to an average of the last
+ * 			few input values, but it weights recent input values much more strongly, and the influence of older
+ * 			input values gradually moves toward zero. You end up with a smooth elastic kind of behaviour
+ * 			as the output drifts toward the current value of the input.
+ *
+ * 			It also ensures responsiveness by reducing accuracy
+ * 			when it is not needed, i.e. when the value changes rapidly.
+ *
+ * PUBLIC FUNCTIONS:
+ * 			int responsive_analog_read(int newValue)
+ *
+ * AUTHOR: Jan Henrik Lenes		LAST CHANGE: 20.03.2017
+ *
+ **************************************************/
 
-#include "headers/responsive_analog_read.h"
+#include <stdlib.h>	// Function: abs()
 
-static const int ANALOG_RESOLUTION = 1001;
+// resolution of values
+static const int ANALOG_RESOLUTION = 1000;
 
 // value between 0 and 1 that controls how smooth the output is
-static const float SNAP_MULTIPLIER = 0.1;
+static const float SMOOTHNESS_MULTIPLIER = 0.3;
 
-/*
- * Calculates a number between 0 and 1 that determines how
- * aggressively to smooth out noise. A high x results in little smoothing.
- */
-float snap_curve(float x)
+/**************************************************
+ * NAME: static float smoothness_cruve(float diff)
+ *
+ * DESCRIPTION:
+ * 			Calculates a number between 0 and 1 that determines how
+ * 			aggressively to smooth out noise. High changes in sensor values
+ * 			require less smothing, i.e. they need to be responsive.
+ *
+ * INPUTS:
+ * 		PARAMETERS:
+ *      	float diff:		The difference in last two sensor values.
+ *     	EXTERNALS:
+ *      	none
+ *
+ * OUTPUTS:
+ * 		PARAMETERS:
+ *      	none
+ *     	RETURN:
+ *        	float:			A smoothness factor in the range 0 to 1.
+ *
+ * AUTHOR: Jan Henrik Lenes		LAST CHANGE: 20.03.2017
+ **************************************************/
+static float smoothness_curve(float diff)
 {
-	float y = 1.0 / (x + 1.0);
+	float y = 1.0 / (diff + 1.0);
 	y = (1.0 - y) * 2.0;
 	if (y > 1.0)
 		return 1.0;
 	return y;
 }
 
-/*
- * Returns the smoothed value based on values received.
- */
+/**************************************************
+ * NAME: responsive_analog_read(int newValue)
+ *
+ * DESCRIPTION:
+ * 			Applies the exponential running average algorithm to values and
+ * 			return the smoothed out value.
+ *
+ * INPUTS:
+ * 		PARAMETERS:
+ *      	float newValue:		The new value which will be smoothed.
+ *      EXTERNALS:
+ *      	none
+ *
+ * OUTPUTS:
+ * 		PARAMETERS:
+ *      	none
+ *     	RETURN:
+ *        	int:				The smoothed value.
+ *
+ * AUTHOR: Jan Henrik Lenes		LAST CHANGE: 20.03.2017
+ **************************************************/
 int responsive_analog_read(int newValue)
 {
-	// the output value, initialized once to 0
+	// the output value, it contains the history of the values
 	static float smoothValue = 0;
 
-	// get difference between new input value and current smooth value
-	float diff = abs((int) ((float) newValue - smoothValue));
+	float diff = abs(newValue - smoothValue);
 
-	// multiply the input by SNAP_MULTIPLER so input values fit the snap curve better.
-	float snap = snap_curve(diff * SNAP_MULTIPLIER);
+	float smoothnessFactor = smoothness_curve(diff * SMOOTHNESS_MULTIPLIER);
 
-	// calculate the exponential moving average based on the snap
-	smoothValue += ((float) newValue - smoothValue) * snap;
+	// calculate the exponential moving average based on the smoothnessFactor
+	smoothValue += (newValue - smoothValue) * smoothnessFactor;
 
 	// ensure output is in bounds
 	if (smoothValue < 0.0)
